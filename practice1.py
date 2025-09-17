44b555dc498b83f6e6fc8badbe408c58ed7ee540
Hosein <mohammadmollaei1379@gmail.com>
2025-09-17 20:41:53 +0330

    updated practice1.py

--- practice1.py
+++ practice1.py
@@ -1,9 +1,22 @@
 class Dog:
-    def __init__(self):
+    species = "canis"
+    def __init__(self, name, age, species):
         self.sound = "woof!"
+        self.name = name
+        self.age = age
+        self.species = species
     def bark(self):
         print(self.sound)
 
-dog_dog = Dog()
+dog_dog = Dog("dog", 2, "body")
+
+print(
+    "sound : ", dog_dog.sound,
+    "name : ", dog_dog.name,
+    "age : ", dog_dog.age,
+    "feature : ", dog_dog.species
+)
+
+
 
 
