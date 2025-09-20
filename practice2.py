6e858bd569fe20a2261f0acb585995b6fff34647
Hosein <mohammadmollaei1379@gmail.com>
2025-09-20 20:29:36 +0330

    update_practice2.py

--- practice2.py
+++ practice2.py
@@ -1,19 +1,25 @@
 class TagCloud:
     def __init__(self):
-        self.tags = {}
+        self._tags = {}
     def add(self, tag):
         tag = tag.lower()
-        self.tags[tag] = self.tags.get(tag, 0) + 1
+        self._tags[tag] = self._tags.get(tag, 0) + 1
     def __getitem__(self, tag):
-        return self.tags.get(tag.lower(), 0)
-
+        return self._tags.get(tag.lower(), 0)
+    def __setitem__(self, tag, count):
+        self._tags[tag.lower()] = count
+    def __len__(self):
+        return len(self._tags)
 
 cloud = TagCloud()
 cloud.add("Python")
 cloud.add("python")
 cloud.add("AI")
 
-print(cloud.tags)
 print(cloud["python"])  
-print(cloud["ai"])      
+print(len(cloud))
+cloud["python"] = 10    
+print(cloud["python"])  
+
+cloud["java"] = 3       
 print(cloud["java"]) 
