5bc2bcc9bf7eb0da8d18c7833a6a647ffdf42f8f
amirhosseinarabpour16-a11y <amirhosseinarabpour16@gmail.com>
2025-09-21 14:45:50 +0330

    update_OS.py

--- Operating_system.py
+++ Operating_system.py
@@ -1,12 +1,11 @@
 import os
 from colorama import init, Fore, Style
-print("""name [version 1.0]
-    name corporation""")
+print("new shell [version 1.0] \nname corporation")
 name_prompt = input("Please enter your prompt name :")
 y = 0
 while y == 0:
     user_input = input(f"C:\\{name_prompt}>")
-    if user_input == "halt" or user_input == "shut down" or user_input == "khamoosh" or user_input == "shutdown":
+    if user_input == "halt" or user_input == "shut down" or user_input == "khamoosh" or user_input == "shutdown" or user_input == "kamosh" or user_input == "poweroff":
         print("""See you later...""")
         break
     elif user_input == "cls" or user_input == "clear" or user_input == "pak" or user_input == "pac":
@@ -25,4 +24,16 @@ while y == 0:
         elif color == "reset":
             print(Style.RESET_ALL + "This is reset text")
         else:
-            print(Style.RESET_ALL + "Sorry, this color is not supported.")
+            print(Style.RESET_ALL + f"Sorry, this {color} is not supported.")
+    elif user_input == "dir" or user_input == "is" or user_input == "Is":
+        path = input("Please enter the directory name :")
+        files = dirs = 0
+        with os.scandir(path) as entries:
+            for entry in entries:
+                if entry.is_file():
+                    files += 1
+                elif entry.is_dir():
+                    dirs += 1
+        print(f"files :{files}, folders :{dirs}")
+    elif user_input == "Game" or user_input == "game" or user_input == "bazi" or user_input == "bazy":
+        pass
