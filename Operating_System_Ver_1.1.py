e66e7420f9c66426aca5bd080c81c038171e83e9
Hosein <mohammadmollaei1379@gmail.com>
2025-09-27 20:28:55 +0330

    update_2_OS_1.1.py

--- Operating_System_Ver_1.1.py
+++ Operating_System_Ver_1.1.py
@@ -8,14 +8,15 @@ while y == 0:
     user_input = input(f"C:\\{name_prompt}>")
     user_input = user_input.lower()
     user_input = user_input.replace(" ", "")
-    if user_input == "halt" or user_input == "khamoosh" or user_input == "shutdown" or user_input == "poweroff" or user_input == "off":
+    user_input = user_input.replace("-", "")
+    if user_input == "halt" or user_input == "shutdown" or user_input == "poweroff" or user_input == "off":
         print("See you later...")
         break
-    elif user_input == "cls" or user_input == "clear" or user_input == "pak":
+    elif user_input == "cls" or user_input == "clear" or user_input == "clearscreen":
         os.system("cls")
-    elif user_input == "color" or user_input == "colors" or user_input == "rang":
+    elif user_input == "color" or user_input == "colors":
         init()
-        color = input("Please enter your desired color(red, green, blue, yellow, reset) :")
+        color = input("Please enter your desired color(red, green, blue, yellow, white) :")
         if color == "red":
             print(Fore.RED + "This is red text")
             os.system("cls")
@@ -28,8 +29,8 @@ while y == 0:
         elif color == "yellow":
             print(Fore.YELLOW + "This is yellow text")
             os.system("cls")
-        elif color == "reset":
-            print(Style.RESET_ALL + "This is reset text")
+        elif color == "white":
+            print(Style.RESET_ALL + "This is white text")
             os.system("cls")
         else:
             print(Style.RESET_ALL + f"Sorry, this {color} is not supported.")
@@ -47,7 +48,10 @@ while y == 0:
         print(user_input[5:])
         if user_input == "echo":
             print()
-    elif user_input == "rd" or user_input == "rmdir" or user_input == "rm" or user_input == "remove-item" or user_input == "del":
+    elif user_input == "copycon":
+        input_1 = input()
+        print(input_1)
+    elif user_input == "rd" or user_input == "rm" or user_input == "removeitem" or user_input == "del":
         folder_path = input("Please enter the directory or file name :")
         if os.path.exists(folder_path):
             confirm = input(f"Are you sure you want to delete the folder '{folder_path}'? (Y/N): ").strip().lower()
