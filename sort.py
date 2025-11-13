f70c24cc79b95f2c924e21933788f68eb0db1d28
amirhosseinarabpour16-a11y <amirhosseinarabpour16@gmail.com>
2025-11-13 14:44:44 +0330

    add_*

--- sort.py
+++ sort.py
@@ -1,24 +1,40 @@
 names = []
 while True:
     menu_1 = (
-    "be barnameh daftarcheh asami khosh amadid! 😊",
-    "1     :     voroud nam jadid",
-    "2     :     didan asami sabt shode",
-    "3     :     khorouj az list"
+    "be barnameh ye daftarcheh ye asami khosh amadid!😊",
+    "1     :     sabt karmand jadid",
+    "2     :     moshahedeh ye asami karmandan sabt shodeh",
+    "3     :     ekhraj karmand",
+    "4     :     khorouj"
     )
     print("\n".join(menu_1))
-    menu_2 = int(input("gozinehg monaseb ra vared konid: "))
-    if menu_2 == 1:
-        name = input("nam khod ra vared konid: ")
-        family = input("nam khanevadegi khod ra vared konid: ")
-        names.append((name, family))
+    menu_2 = input("gozineh ye mored nazar ra vared konid :")
+    if menu_2 == "1":
+        num = input("shomareh ye mored nazar ra vared konid :")
+        name = input("nam khod ra vared konid :")
+        family = input("nam khanevedegi khod ra vared konid :")
         with open("sort.txt", "a", encoding="utf-8") as file:
-            for name, family in names:
-                file.write((f"{name} {family}\n"))
-    elif menu_2 == 2:
-        with open("sort.txt", "r", encoding="utf-8") as file:
-            lines = file.read()
-            sorted(lines)
-            print(lines)
-    elif menu_2 == 3:
+                file.write((f"{num} {name} {family} \n")) 
+    elif menu_2 == "2":
+        try:
+            with open("sort.txt", "r", encoding="utf-8") as file:
+                lines = file.readlines()
+                print("\n".join(lines))
+                lines.sort() 
+        except FileNotFoundError:
+             print("hanouz faily sakhte nashodeh!")
+    elif menu_2 == "3":
+            with open("sort.txt", "r", encoding="utf-8") as file:
+                lines = file.readlines() 
+                print("\n".join(lines))
+            rem = input("shomareh ye karmand ekhraj shode ra vared konid :")
+            rem_line = [line for line in lines if line.split()[0] != rem]
+            with open("sort.txt", "w", encoding="utf-8") as file:
+                file.writelines(rem_line)
+                lines.sort()
+    elif menu_2 == "4":
         break
+    elif menu_2 == "":
+        pass
+    else:
+         break
---
+++ takhfif.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+import os
+from openpyxl import Workbook, load_workbook
+root = tk.Tk()
+
+label = tk.Label(root, text="لطفا نام و نام خانوادگی خود را وارد کنید", bg="lightgreen", fg="red", font=("B Titr", 18))
+label.pack()
+
+entry_1 = tk.Entry(root, width=50, font=("B Nazanin", 20))
+entry_1.pack(pady=2)
+
+label = tk.Label(root, text="لطفا شماره خود را وارد کنید", bg="lightgreen", fg="red", font=("B Titr", 18))
+label.pack()
+
+entry_2 = tk.Entry(root, width=25, font=("B Nazanin", 20))
+entry_2.pack(pady=2)
+
+label = tk.Label(root, text="اینجا هم تأیید کنید که مشتری ثابت هستید یا مشتری گذری", bg="lightgreen", fg="red", font=("B Titr", 18))
+label.pack()
+
+rad_btn = tk.StringVar()
+rb_1 = tk.Radiobutton(root, text="مشتری ثابت", variable=rad_btn, value="مشتری ثابت")
+rb_2 = tk.Radiobutton(root, text="مشتری گذری", variable=rad_btn, value="مشتری گذری")
+rb_1.pack()
+rb_2.pack()
+
+def click():
+    content_1 = entry_1.get()
+    content_2 = entry_2.get()
+    selected_rd_btn = rad_btn.get()
+    wb = Workbook()
+    ws = wb.active
+    try:
+        name, family_1, family_2 = content_1.split()
+        ws.append([name, family_1 + family_2, content_2, selected_rd_btn])
+        wb.save("takhfif.xlsx")
+    except ValueError:
+        error_label = tk.Label(root, text="ورودی باید شامل نام، نام خانوادگی و شماره تلفن باشد.", fg="red", bg="lightblue", font=("B Nazanin", 14))
+        error_label.pack()
+
+
+button = tk.Button(root, text="ثبت نام",bg="lightyellow", font=("B Titr", 18), command=click)
+button.pack()
+
+root.title("برنامه جامع مشتری")
+
+root.geometry("1000x800+400+100")
+
+root.resizable(False, False)
+
+root.configure(bg="lightblue")
+
+root.mainloop()
