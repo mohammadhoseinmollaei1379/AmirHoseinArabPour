71f0cf2cf43fae142a1c16747e07ed124dfaa5cc
amirhosseinarabpour16-a11y <amirhosseinarabpour16@gmail.com>
2026-01-23 14:20:28 +0330

    update_*_&_add_*

--- C++_Projects/amoozeshi.cpp
+++ C++_Projects/amoozeshi.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-    float radius, area, periphery;
-
-    float pi = 3.14;
-
-    cout << " Plaese Enter a Radius :" << '\a';
-    cin >> radius;
-    periphery = ( radius * 2 ) * pi;
-    area = ( radius * radius ) * pi;
-    cout << " Periphery : " << periphery << "\n"
-         << " Area : " << area << endl;
+    const int Count = 10;
+    for (int number = 1; number <= Count; ++number) {
+        for (int num = 1; num <= number; ++num) {
+            std::cout << number * num << ' ';
+        }
+        std::cout << '\n';
+    }
     return 0;
 }
