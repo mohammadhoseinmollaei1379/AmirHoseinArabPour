26e64254bd5c50e461998abc53c932e8fa0b06ee
amirhosseinarabpour16-a11y <amirhosseinarabpour16@gmail.com>
2026-02-19 15:49:25 +0330

    update

--- JS_Projects/app.js
+++ JS_Projects/app.js
@@ -1,3 +1,16 @@
-const qr = require('qrcode')
+const express = require('express')
+const app = express()
 
-qr.toFile('qrcode.png', 'https://eitaa.com/joinchat/3136094899C98907da881', {width: 1200})
+const port = 3000
+app.listen(port, function(){
+    console.log('server is running at http://localhost:' + port)
+}) 
+
+app.get('/', function(req, res){
+    res.send('<h1>Welcome 🤣</h1> <marquee direction="right">سرور در حال اجرا است</marquee>');
+})
+
+app.get('/api/hello/:name', function(req, res){
+    let name = req.params.name;
+    res.send('hello ' + name);
+})
